8b3a409b6319dfeb7328560dd04c21f7aa1b1ac1
Diego Acosta <diego.acosta@externos.bci.cl>
2025-02-16 18:44:48 -0500

    Ajustes controllers

---
+++ src/main/java/com/mechanical/workshops/controllers/SingUpController.java
@@ -0,0 +1,49 @@
+package com.mechanical.workshops.controllers;
+
+
+import com.mechanical.workshops.dto.UserSaveRequestDTO;
+import com.mechanical.workshops.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/public/users")
+@AllArgsConstructor
+public class SingUpController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody @Valid UserSaveRequestDTO userSaveRequestDTO) {
+        return userService.register(userSaveRequestDTO);
+    }
+
+    @GetMapping("/validate/phone")
+    public ResponseEntity<String> validatePhone(@RequestParam(required = false) UUID userId, @RequestParam String phone) {
+        return userService.validatePhone(userId, phone);
+    }
+
+    @GetMapping("/validate/email")
+    public ResponseEntity<String> validateEmail(@RequestParam(required = false) UUID userId, @RequestParam String email) {
+        return userService.validateEmail(userId, email);
+    }
+
+    @GetMapping("/validate/username")
+    public ResponseEntity<String> validateUsername(@RequestParam(required = false) UUID userId, @RequestParam String username) {
+        return userService.validateUsername(userId, username);
+    }
+
+    @GetMapping("/validate/identification")
+    public ResponseEntity<String> validateIdentification(@RequestParam(required = false) UUID userId, @RequestParam String identification) {
+        return userService.validateIdentification(userId, identification);
+    }
+
+    @GetMapping("/validate/value-identification")
+    public ResponseEntity<String> validateValueIdentification(@RequestParam String value) {
+        return userService.validateValueIdentification(value);
+    }
+}
--- src/main/java/com/mechanical/workshops/controllers/UsersController.java
+++ src/main/java/com/mechanical/workshops/controllers/UsersController.java
@@ -1,18 +1,13 @@
 package com.mechanical.workshops.controllers;
 
 import com.mechanical.workshops.dto.PageResponseDto;
-import com.mechanical.workshops.dto.UserResponseDto;
 import com.mechanical.workshops.dto.UserSaveRequestDTO;
-import com.mechanical.workshops.models.User;
 import com.mechanical.workshops.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -29,11 +24,6 @@ public class UsersController {
         return userService.getAllUserActive( text, page, size);
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody @Valid UserSaveRequestDTO userSaveRequestDTO) {
-        return userService.register(userSaveRequestDTO);
-    }
-
     @PutMapping("/update/{userId}")
     public ResponseEntity<String> updateUser(@RequestBody UserSaveRequestDTO userSaveRequestDTO, @PathVariable UUID userId) {
         return userService.update(userId, userSaveRequestDTO);
@@ -42,31 +32,6 @@ public class UsersController {
     @DeleteMapping("/delete/{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable UUID userId) {
         return userService.delete(userId);
-    }
-
-    @GetMapping("/validate/phone")
-    public ResponseEntity<String> validatePhone(@RequestParam(required = false) UUID userId, @RequestParam String phone) {
-        return userService.validatePhone(userId, phone);
-    }
-
-    @GetMapping("/validate/email")
-    public ResponseEntity<String> validateEmail(@RequestParam(required = false) UUID userId, @RequestParam String email) {
-        return userService.validateEmail(userId, email);
-    }
-
-    @GetMapping("/validate/username")
-    public ResponseEntity<String> validateUsername(@RequestParam(required = false) UUID userId, @RequestParam String username) {
-        return userService.validateUsername(userId, username);
-    }
-
-    @GetMapping("/validate/identification")
-    public ResponseEntity<String> validateIdentification(@RequestParam(required = false) UUID userId, @RequestParam String identification) {
-        return userService.validateIdentification(userId, identification);
-    }
-
-    @GetMapping("/validate/value-identification")
-    public ResponseEntity<String> validateValueIdentification(@RequestParam String value) {
-        return userService.validateValueIdentification(value);
     }
 
 }
